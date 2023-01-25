a885832beee9ce9daa777fe68ce1fecdbe69c31e
Atty <andrewkuzkin1@gmail.com>
2023-01-25 22:39:10 +0300

    Add index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+import { expect, beforeEach, test } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+let result;
+
+beforeEach(() => {
+  result = readFile('expected_file.txt');
+});
+
+test('main flow', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  expect(genDiff(file1, file2)).toEqual(result);
+});
