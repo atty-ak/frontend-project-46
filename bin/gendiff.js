a7cb0f8fa319ea0655aa755023dc3d410e4f9050
Atty <andrewkuzkin1@gmail.com>
2023-01-25 17:37:51 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
-  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath1>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
