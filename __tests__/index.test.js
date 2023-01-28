872a6e3ac727a6a4d5bf8edceea2bf7cbde68c6d
Atty <andrewkuzkin1@gmail.com>
2023-01-28 20:49:51 +0300

    Update file1.yml and file2.yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import { expect, beforeEach, test } from '@jest/globals';
+import { expect, describe, test } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,20 +10,17 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let result;
+const tests = [
+  ['file1.json', 'file2.json', 'expected_file.txt', 'stylish'],
+  ['file1.yml', 'file2.yml', 'expected_file.txt', 'stylish'],
+];
 
-beforeEach(() => {
-  result = readFile('expected_file.txt');
-});
-
-test('json format', () => {
-  const file1 = getFixturePath('filepath1.json');
-  const file2 = getFixturePath('filepath2.json');
-  expect(genDiff(file1, file2)).toEqual(result);
-});
-
-test('yml format', () => {
-  const file1 = getFixturePath('filepath1.yml');
-  const file2 = getFixturePath('filepath2.yml');
-  expect(genDiff(file1, file2)).toEqual(result);
+describe('check for correct diff', () => {
+  test.each(tests)('Compare files', (firstData, secondData, expectedData, format) => {
+    const firstFile = getFixturePath(firstData);
+    const secondFile = getFixturePath(secondData);
+    const getResult = readFile(expectedData);
+    const result = genDiff(firstFile, secondFile, format);
+    expect(result).toEqual(getResult);
+  });
 });
