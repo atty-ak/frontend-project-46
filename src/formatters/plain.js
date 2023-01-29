d00a3e9d03fe4547113f291d11ffe7431780f9d1
Atty <andrewkuzkin1@gmail.com>
2023-01-29 19:26:19 +0300

    Add plain.js

---
+++ src/formatters/plain.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+const stringify = (value) => {
+  if (_.isObject(value)) return '[complex value]';
+  if (_.isString(value)) return `'${value}'`;
+  return value;
+};
+
+const plain = (tree) => {
+  const iter = (currentValue, ancestry) => {
+    const lines = currentValue.flatMap((node) => {
+      const {
+        key, type, value, children, oldValue, newValue,
+      } = node;
+      const countAncestry = `${ancestry}${key}.`;
+      switch (type) {
+        case 'nested':
+          return iter(children, countAncestry);
+        case 'removed':
+          return `Property '${ancestry}${key}' was removed`;
+        case 'added':
+          return `Property '${ancestry}${key}' was added with value: ${stringify(value)}`;
+        case 'changed':
+          return `Property '${ancestry}${key}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
+        case 'unchanged':
+          return null;
+        default:
+          throw new Error(`Unknown type ${type}`);
+      }
+    });
+    return lines.filter((item) => item !== null).join('\n');
+  };
+  return iter(tree, '');
+};
+
+export default plain;
