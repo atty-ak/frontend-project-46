40e660f5adae79df795768eeb292a16db440a197
Atty <andrewkuzkin1@gmail.com>
2023-01-29 19:27:08 +0300

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import process from 'process';
 import fs from 'fs';
 import path from 'path';
 import parsers from './parsers.js';
-import buildASTtree from './ASTtree.js';
+import buildTree from './ASTtree.js';
 import format from './formatters/formats.js';
 
 const getPathFile = (filepath) => path.resolve(process.cwd(), filepath);
@@ -16,7 +16,7 @@ const genDiff = (filepath1, filepath2, formats = 'stylish') => {
   const file2Ext = getFormatName(filepath2);
   const obj1 = parsers(file1Data, file1Ext);
   const obj2 = parsers(file2Data, file2Ext);
-  const difference = buildASTtree(obj1, obj2);
+  const difference = buildTree(obj1, obj2);
   return format(difference, formats);
 };
 
