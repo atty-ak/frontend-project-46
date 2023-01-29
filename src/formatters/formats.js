a73e32ce97152b4d56d959357cd9caf33a99634b
Atty <andrewkuzkin1@gmail.com>
2023-01-29 19:30:14 +0300

    Update formats.js

--- src/formatters/formats.js
+++ src/formatters/formats.js
@@ -1,9 +1,12 @@
+import plain from './plain.js';
 import stylish from './stylish.js';
 
 export default (diff, format) => {
   switch (format) {
     case 'stylish':
       return stylish(diff);
+    case 'plain':
+      return plain(diff);
     default:
       throw new Error(`Format not supported: ${format}`);
   }
