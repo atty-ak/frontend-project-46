7b13383b5b0246b742ec689040025df607a24aa1
Atty <andrewkuzkin1@gmail.com>
2023-01-29 19:31:13 +0300

    Fixed gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ program
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format (default: "stylish")', 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, { format }) => {
     console.log(genDiff(filepath1, filepath2, format));
   });
