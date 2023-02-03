2d7e716f596e93f1c0f9baa1f4fe3f4e146e2f32
Atty <andrewkuzkin1@gmail.com>
2023-02-03 14:54:46 +0300

    Refactoring index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,17 +11,18 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const tests = [
-  ['json', 'json'],
-  ['yml', 'yml'],
+  ['json'],
+  ['yml'],
+  ['yaml'],
 ];
 
 describe('check for correct diff', () => {
   const expectedStylish = readFile('expected_nested.txt');
   const expectedPlain = readFile('expected_plain.txt');
   const expectedJson = readFile('expected_file.json');
-  test.each(tests)('Compare files', (extension1, extension2) => {
+  test.each(tests)('Compare files', (extension1) => {
     const file1 = getFixturePath(`file1.${extension1}`);
-    const file2 = getFixturePath(`file2.${extension2}`);
+    const file2 = getFixturePath(`file2.${extension1}`);
     expect(genDiff(file1, file2, 'stylish')).toEqual(expectedStylish);
     expect(genDiff(file1, file2, 'plain')).toEqual(expectedPlain);
     expect(genDiff(file1, file2, 'json')).toEqual(expectedJson);
@@ -32,4 +33,9 @@ describe('check for correct diff', () => {
 test('unknown format', () => {
   expect(() => genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'unknownFormat'))
     .toThrow('Format not supported: unknownFormat');
+});
+
+test('unknown extension', () => {
+  expect(() => genDiff('__fixtures__/expected_nested.txt', '__fixtures__/expected_plain.txt'))
+    .toThrow('Extension not supported: txt');
 });
